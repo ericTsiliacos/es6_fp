be8fc8992b2c25d891fdc8274d66eceaf91aa6c7
Eric Tsiliacos <erictsiliacos@gmail.com>
2017-01-07 23:35:53 -0800

    Add PromiseIO .then semantics

--- module_system.js
+++ module_system.js
@@ -11,10 +11,47 @@ const reverse = x => [...x].reverse().join('')
 
 const sequence = (...actions) => () => actions.forEach(action => action())
 const puts = value => () => console.log(value)
+const lift = value => () => value
+
+const PromiseIO = promise => ({
+  then: f => PromiseIO(promise.then(result => f(result)())),
+  finally: f => () => PromiseIO(promise.then(result => f(result)()))
+})
+
+const Result = ({ left, right }) => {
+  return Object.assign(
+    {},
+    left ?
+      {
+        map: () => Result({left, right: null}),
+      }
+    :
+      {
+        map: f => Result({left: null, right: f(right)}),
+      },
+    {
+      left: () => left,
+      right: () => right
+    })
+}
+
+const readFile = () => (
+  PromiseIO(new Promise(resolve => {
+    require('fs').readFile('test.txt', 'utf8', (err, data) => {
+       return err
+        ? resolve(Result({ left: err, right: null }))
+        : resolve(Result({ left: null, right: data }));
+    });
+  }))
+)
 
 const main = sequence(
     puts(Pipe("hi").into(concat("world!")))
   , puts(Pipe(5).into(square, toString, reverse, concat("!")))
-)
+  , readFile()
+    .then(result => lift(result.map(i => i + "GOT HERE!")))
+    .then(result => lift(result.map(i => i + "\n" + "THIS TWO!")))
+    .finally(result => result.right() ? puts(result.right()) : puts(result.left()))
+  )
 
 main();
