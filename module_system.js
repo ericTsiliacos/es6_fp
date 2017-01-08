0f262c38faf1f55629a07df040e1a44f52fe082b
Eric Tsiliacos <erictsiliacos@gmail.com>
2017-01-07 18:06:04 -0800

    remove cruft

--- module_system.js
+++ module_system.js
@@ -1,58 +1,20 @@
-const Pipe = x => {
-  const compose = f => g => x => f(g(x))
-  return {
-    into: (...xs) => xs.reduce((acc, cur) => compose(cur)(acc), x => x)(x)
-  }
-}
+const compose = f => g => x => g(f(x))
+
+const Pipe = x => ({
+  into: (...xs) => xs.reduce((accu, curr) => compose(accu)(curr), x => x)(x)
+})
 
 const concat = x => y => y + x
 const square = x => x * x
 const toString = x => x + ""
 const reverse = x => [...x].reverse().join('')
 
-// const main = sequence(
-//     puts(Pipe("hi").into(concat("world!")))
-//   , puts(Pipe(5).into(square, toStr, rev))
-// )
-//
-// main();
+const sequence = (...actions) => () => actions.forEach(action => action())
+const puts = value => () => console.log(value)
 
-const _IO = {
-  sequence: (...actions) => () => actions.forEach(action => action()),
-  puts: value => () => console.log(value),
-  lift: value => () => value,
-  then: thunk => f => f(thunk())
-}
+const main = sequence(
+    puts(Pipe("hi").into(concat("world!")))
+  , puts(Pipe(5).into(square, toString, reverse, concat("!")))
+)
 
-// const { sequence, puts, getLine, lift, then } = _IO
-
-const { puts } = _IO
-
-const _IO_ = thunk => {
-  return {
-    then: f => _IO_(f(thunk())),
-    run: lift(thunk)()
-  }
-}
-
-const lift = value => () => value
-
-const getLine = () => {
-  const readline = require('readline');
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  return _IO_(lift(new Promise(resolve => {
-    rl.on('line', input => {
-      return resolve(input)
-    })
-  })));
-}
-
-const IO = value => _IO_(lift(value))
-
-const main = getLine() //.then(p => lift(p.then(value => value)))
-
-// main.run();
+main();
