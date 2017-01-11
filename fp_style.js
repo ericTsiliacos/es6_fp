775ec56116d01f3b7a4b93d82d6a368e7893fd70
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-10 17:29:54 -0800

    Condense creation of fetchAsyncIO function

--- fp_style.js
+++ fp_style.js
@@ -2,14 +2,13 @@ const { Http } = require("./http");
 const { AsyncIO, puts, pipe, compose, props, liftF } = require("./async_io");
 const { mapResult } = require("./result");
 
-const main = ({ fetch, puts, url }) => fetch(url)
+const main = ({ fetchAsyncIO, puts }) => fetchAsyncIO
   .then(liftF(mapResult(props("people", 1))))
   ._thread_(puts("Printing name twice:"))
   .sequence(pipe(props("right"), puts), pipe(props("right"), puts))
   ._sequence(puts("Finished"));
 
 main({
-  fetch: AsyncIO.from(Http.get),
-  puts,
-  url: "http://uinames.com/api/"
+  fetchAsyncIO: AsyncIO.from(Http.get)("http://uinames.com/api/"),
+  puts
 }).run();
