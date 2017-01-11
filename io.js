5e05e0b9a7b3d62c9d6a26e05ba68322cce3d473
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-11 10:38:19 -0800

    Add chaining that includes previous output as arg to next then

--- io.js
+++ io.js
@@ -1,12 +1,12 @@
 const IO = {
   chaining: f => ({
     run: f,
-    then: thunk => IO.chaining(() => {
-      f();
-      thunk().run();
+    then: thunk => IO.chaining((...values) => {
+      const value = f(values);
+      thunk(value).run();
     })
   }),
-  of: f => value => IO.chaining(() => f(value))
+  of: f => (...values) => IO.chaining(() => f(...values))
 };
 
 module.exports = { IO };
