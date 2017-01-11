18eadce6a7c4f57a906b2d1ccf99227ebda6f4e1
Eric Tsiliacos and Wiley Kestner <pair+etsiliacos+wkestner@pivotal.io>
2017-01-10 17:31:30 -0800

    rename function

--- fp_style.js
+++ fp_style.js
@@ -2,13 +2,15 @@ const { Http } = require("./http");
 const { AsyncIO, puts, pipe, compose, props, liftF } = require("./async_io");
 const { mapResult } = require("./result");
 
-const main = ({ fetchAsyncIO, puts }) => fetchAsyncIO
+const fetchAndPrintNames = ({ fetchAsyncIO, puts }) => fetchAsyncIO
   .then(liftF(mapResult(props("people", 1))))
   ._thread_(puts("Printing name twice:"))
   .sequence(pipe(props("right"), puts), pipe(props("right"), puts))
   ._sequence(puts("Finished"));
 
-main({
+const main = fetchAndPrintNames({
   fetchAsyncIO: AsyncIO.from(Http.get)("http://uinames.com/api/"),
   puts
-}).run();
+});
+
+main.run();
