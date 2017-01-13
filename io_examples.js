876ecae91868b2620e0f3e9a11e7c8c542cad791
Eric Tsiliacos and Tim Jarratt <pair+etsiliacos+tjarratt@pivotal.io>
2017-01-13 15:10:49 -0800

    Show testability

--- io_examples.js
+++ io_examples.js
@@ -1,18 +1,28 @@
 const { IO } = require("./io");
 const { pipe, props } = require("./async_io");
 const { Result } = require("./result");
+const assert = require("assert");
 
 const print = IO.of(console.log);
 const getLine = IO.of(() => "hello world!");
 
-const main = print("starting")
+const main = ({ print }) => print("starting")
   .then(() => print("next"))
   .then(() => IO.lift({ people: [ "Eric" ] }))
   .then(pipe(props("people", 0), print))
   .then(() => print("all done!"));
 
-main.run();
+const log = [];
+main({ print: IO.of((...values) => log.push(values)) }).run();
 
+assert.deepStrictEqual(log, [
+  [ "starting" ],
+  [ "next" ],
+  [ "Eric" ],
+  [ "all done!" ]
+]);
+
+main({ print }).run();
 const performAsyncIO = IO.of(() => Promise.resolve(Result({ right: 1 })));
 
 performAsyncIO().then(p => {
